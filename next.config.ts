dabb980add2556781ad6ec064a18454f62578393
kackey621 <a.kusama@icloud.com>
2026-02-10 00:22:59 +0900

    config fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 // @ts-ignore
+const runtimeCaching = require("next-pwa/cache");
+
+// @ts-ignore
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
